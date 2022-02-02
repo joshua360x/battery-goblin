0c3c2220fa43596b393dae25263b4f880ee2651b
joshua360x <jkw5499@psu.edu>
2022-02-02 18:40:53 -0500

    configured search function

--- src/App.js
+++ src/App.js
@@ -50,8 +50,13 @@ function App() {
 
   function handleFilterGoblins(search) {
     // use the filter method to get an array of goblins whose name includes this search argument
+    const filter = allGoblins.filter(goblin => goblin.name === search);
     // if there is a search argument, set the filtered goblins to the filtered goblins
+    if (search) {
+      setFilteredGoblins(filter);
+    }
     // if the search argument is undefined, set the filtered goblins in state to just be the array of all goblins
+
   }
 
   return (

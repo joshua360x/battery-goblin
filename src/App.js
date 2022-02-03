6ad058cfc52d2215fa66bc56f1751baa2131e5c8
joshua360x <jkw5499@psu.edu>
2022-02-02 20:30:43 -0500

    rendered array for properties

--- src/App.js
+++ src/App.js
@@ -102,7 +102,7 @@ function App() {
         setGoblinFormHP={setGoblinFormHP}
       />
       <GoblinList
-        goblins={[]} // this takes in an array of goblins. If the filteredGoblins has a length, use that array. Otherwise, use the allGoblins array
+        goblins={filteredGoblins.length > 0 ? filteredGoblins : allGoblins} // this takes in an array of goblins. If the filteredGoblins has a length, use that array. Otherwise, use the allGoblins array
         handleDeleteGoblin={handleDeleteGoblin} // note that the goblin list has access to the ability to delete
       />
     </div>
